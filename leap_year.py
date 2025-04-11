488493e1de6e2a3a4efb368ace3d21ce11c81af9
Rozas Agustin <107058124+Skintblock@users.noreply.github.com>
2025-04-11 11:06:05 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -2,6 +2,7 @@
 #TP4 - Rozas Miguel Agustin - Introduccion a la programacion 1
 # Ejercicio 2
 #
+#
 def leap_year():
     year = int(input("Ingrese un año: "))
     if (year % 4 == 0 and year % 100 != 0) or (year % 100 == 0 and year % 400 == 0):
