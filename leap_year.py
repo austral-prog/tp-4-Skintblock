34cb097b7da7f440174c2fea7b0e48f41d9f9d8b
Rozas Agustin <107058124+Skintblock@users.noreply.github.com>
2025-04-11 10:55:17 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,9 @@
+#11/4/2025
+#TP4 - Rozas Miguel Agustin - Introduccion a la programacion 1
+# Ejercicio 2
 def leap_year():
-    print("TO DO")
+    year = int(input("Ingrese un año: "))
+    if (year % 4 == 0 and year % 100 != 0) or (year % 100 == 0 and year % 400 == 0):
+        print("El año", year, "es bisiesto")
+    else:
+        print("El año", year, "no es bisiesto") 
